5786da6d73e7e4ef5e928cc0995ae253dba7ae48
jonathanloungani <jloungan@andrew.cmu.edu>
2022-04-20 20:08:40 -0400

    Added midifile library and test files

---
+++ midi-test.cpp
@@ -0,0 +1,14 @@
+#include "midifile/MidiFile.h"
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+using namespace smf;
+
+int main() {
+    MidiFile midifile;
+    (void) midifile;
+
+    return 0;
+}
