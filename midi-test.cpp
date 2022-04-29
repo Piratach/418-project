aba218a117e9630971d672984d99a8755e0b8a40
Jonathan Cohen Loungani <jloungan@linux-33.andrew.cmu.edu>
2022-04-29 09:11:58 -0400

    Implemented reading notes from midi file in midi-test.cpp

--- midi-test.cpp
+++ midi-test.cpp
@@ -7,11 +7,48 @@
 // using namespace std;
 // using namespace smf;
 
-int main() {
-    smf::MidiFile midifile;
-    (void) midifile;
-    printf("Hello World!\n");
-    printf("Jonny\n");
+std::vector<int> getPitches(const char *filepath, double &tempoBPM, char &keySignature) {
+    printf("Reading file: %s...\n", filepath);
+    smf::MidiFile midifile("test.mid");
+    smf::MidiEventList track = midifile[0];
+    std::vector<int> noteNumbers;
+    int count = 0;
+    for (int i = 0; i < track.getSize(); i++) {
+        smf::MidiEvent event = track[i];
+        if (event.isNoteOn()) {
+           int pitch = event.getKeyNumber();
+           noteNumbers.push_back(pitch);
+        }
+        else if (event.isMeta()) {
+            if (event.isTempo()) {
+                tempoBPM = event.getTempoBPM();
+            }
+            else if (event[1] == 0x59) { // is key signature message
+                keySignature = event[3]; // number of flats/sharps from -7 through 7 (flats negative, sharps positive)
+            }
+        }
+    }
+    return noteNumbers;
+}
 
+void getNoteName(int noteNumber, char *result) {
+    int octave = noteNumber / 12 - 1; // 21...23 => 0, 24...35 => 1, 36...47 => 2
+    char const *noteNames[12] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+    char const *noteName = noteNames[noteNumber % 12];
+    sprintf(result, "%s%d", noteName, octave);
+}
+
+int main() { 
+    const char *filepath = "test.mid";
+    double tempoBPM;
+    char keySignature;
+    std::vector<int> noteNumbers = getPitches(filepath, tempoBPM, keySignature);
+    printf("Tempo (BPM): %f\n", tempoBPM);
+    printf("Key Signature: %d\n", keySignature);
+    for (int i = 0; i < noteNumbers.size(); i++) {
+        char *noteName = (char *) malloc(4);
+        getNoteName(noteNumbers[i], noteName);
+        printf("Pitch %d: %s\n", i, noteName);
+    }
     return 0;
 }
