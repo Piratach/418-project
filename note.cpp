57b460d39cef3da730d02f6421097a57bdf48e3c
Jonathan Cohen Loungani <jloungan@linux-15.andrew.cmu.edu>
2022-05-02 17:16:03 -0400

    Updated specification for note

--- note.cpp
+++ note.cpp
@@ -1,31 +1,20 @@
 
 #include "note.h"
 
-AbsolutePitch::PitchClass::PitchClass(PitchLetter _letter, AccidentalType _accidental) {
-    letter = _letter;
-    accidental = _accidental;
-}
-
-AbsolutePitch::AbsolutePitch(PitchClass _pitch, uint8_t _absoluteOctave) : pitchType(_pitch.letter, _pitch.accidental) {
-    absoluteOctave = _absoluteOctave;
-}
-
 Note::Note(uint8_t _scaleDegree, uint8_t _relativeOctave) {
     scaleDegree = _scaleDegree;
     relativeOctave = _relativeOctave;
 }
 
-Note::Note(AbsolutePitch::PitchClass key, AbsolutePitch pitch) {
-    if (pitch.pitchType.letter > key.letter) {
-        scaleDegree = pitch.pitchType.letter - key.letter;
-    } else {
-        scaleDegree = key.letter - pitch.pitchType.letter;
-    }
-
-    // TODO: Fix relativeOctave
-    relativeOctave = pitch.absoluteOctave;
+Note::Note(int key, int midiNumber) {
+   // TODO: everything 
 }
 
+int toMidiNumber(int key) {
+    return -1;
+}
+
+// TODO: operator overload >, (and >=, <, <= if it's easy)
 bool Note::isHigherThan(Note &note) {
     return relativeOctave > note.relativeOctave || 
         (relativeOctave == note.relativeOctave && 
--- note.h
+++ note.h
@@ -6,32 +6,11 @@
 // scale degree (relative to key, between 1-7 inclusive)
 // octave (relative to key/vocal ranges, between 0-6)
 
-struct AbsolutePitch {
-    struct PitchClass {
-        enum PitchLetter { C, D, E, F, G, A, B };
-        enum AccidentalType {
-            FLAT,
-            NATURAL,
-            SHARP
-        };
-
-        PitchClass(PitchLetter _letter, AccidentalType _accidental);
-
-        PitchLetter letter;
-        AccidentalType accidental;
-    };
-
-    AbsolutePitch(PitchClass _pitch, uint8_t _absoluteOctave);
-
-    PitchClass pitchType;
-    uint8_t absoluteOctave;
-};
-
 struct Note {
     Note(uint8_t _scaleDegree, uint8_t _relativeOctave);
-    Note(AbsolutePitch::PitchClass key, AbsolutePitch pitch);
+    Note(int key, int midiNumber);
 
-    AbsolutePitch toAbsolutePitch(AbsolutePitch::PitchClass key);
+    int toMidiNumber(int key);
 
     bool isHigherThan(Note &note);
 
