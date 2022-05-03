29352582a43085e951f7e2c1ea291dbdf2bf6819
Jonathan Cohen Loungani <jloungan@linux-23.andrew.cmu.edu>
2022-05-03 02:13:48 -0400

    Wrote a sketchy implementation of note conversion

--- note.cpp
+++ note.cpp
@@ -1,13 +1,24 @@
 
 #include "note.h"
+#include <unordered_map>
 
 Note::Note(uint8_t _scaleDegree, uint8_t _relativeOctave) {
     scaleDegree = _scaleDegree;
     relativeOctave = _relativeOctave;
 }
 
+std::unordered_map<int, int> intervalToDegree = {
+    {0, 1}, {2, 2}, {4, 3}, {5, 4}, {7, 5}, {9, 6}, {11, 7}
+};
+
+std::unordered_map<int, int> degreeToInterval = {
+    {1, 0}, {2, 2}, {3, 4}, {4, 5}, {5, 7}, {6, 9}, {7, 11}
+};
+
 Note::Note(int key, int midiNumber) {
-   // TODO: everything 
+   int offset = (key * 7) % 12;   
+   scaleDegree = intervalToDegree[(midiNumber + offset) % 12];
+   relativeOctave = ((midiNumber - 24) + offset) / 12;
 }
 
 // TODO: operator overload >, (and >=, <, <= if it's easy)
@@ -18,5 +29,7 @@ bool Note::operator>(const Note &note) {
 }
 
 int Note::toMidiNumber(int key) {
-    return -1;
+    int offset = (key * 7) % 12;
+    return relativeOctave * 12 + 24 - offset;
+
 }
