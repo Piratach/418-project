80ed6f027286b37e78f0887812293e9967c66ada
jonathanloungani <jloungan@andrew.cmu.edu>
2022-05-04 20:29:35 -0400

    Fixed conversion between midi numbers and Note

--- note.cpp
+++ note.cpp
@@ -2,11 +2,11 @@
 #include "note.h"
 #include <unordered_map>
 
-std::unordered_map<int, int> intervalToDegree = {
+std::unordered_map<int, int> halfStepsToScaleDegree = {
     {0, 1}, {2, 2}, {4, 3}, {5, 4}, {7, 5}, {9, 6}, {11, 7}
 };
 
-std::unordered_map<int, int> degreeToInterval = {
+std::unordered_map<int, int> scaleDegreeToHalfSteps = {
     {1, 0}, {2, 2}, {3, 4}, {4, 5}, {5, 7}, {6, 9}, {7, 11}
 };
 
@@ -16,10 +16,12 @@ Note::Note(uint8_t _scaleDegree, uint8_t _relativeOctave) {
 }
 
 Note Note::fromMidiNumber(int key, int midiNumber) {
-   int offset = (key * 7) % 12;   
-   int newScaleDegree = intervalToDegree[(midiNumber - offset) % 12];
-   int newRelativeOctave = ((midiNumber - 24) + offset) / 12;
-   return Note(newScaleDegree, newRelativeOctave);
+   int keyOffset = (key * 7) % 12;   
+   int normalizedMidiNumber = midiNumber - keyOffset;
+   int halfSteps = normalizedMidiNumber % 12;
+   int scaleDegree = halfStepsToScaleDegree[halfSteps];
+   int octave = normalizedMidiNumber / 12 - 1;
+   return Note(scaleDegree, octave);
 }
 
 std::string Note::toString() {
@@ -57,7 +59,8 @@ Note Note::operator++() {
 }
 
 int Note::toMidiNumber(int key) {
-    int offset = (key * 7) % 12;
-    return relativeOctave * 12 + 24 + offset;
-
+    int halfSteps = scaleDegreeToHalfSteps[scaleDegree];
+    int normalizedMidiNumber = 12 * (relativeOctave + 1) + halfSteps;
+    int keyOffset = (key * 7) % 12;
+    return normalizedMidiNumber + keyOffset;
 }
